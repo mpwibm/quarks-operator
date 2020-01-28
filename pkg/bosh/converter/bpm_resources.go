d59c5f4e09bfd647b4b5ddfb3ad1737d9679c5ca
André Duffeck <aduffeck@suse.com>
2020-01-28 11:54:54 +0100

    Parse active/passive probes from the manifest
    Signed-off-by: Dimitris Karakasilis <dkarakasilis@suse.com>

--- pkg/bosh/converter/bpm_resources.go
+++ pkg/bosh/converter/bpm_resources.go
@@ -139,6 +139,7 @@ func (kc *KubeConverter) serviceToQuarksStatefulSet(
 		Spec: qstsv1a1.QuarksStatefulSetSpec{
 			Zones:                instanceGroup.AZs,
 			UpdateOnConfigChange: true,
+			ActivePassiveProbe:   instanceGroup.Properties.Quarks.ActivePassiveProbe,
 			Template: appsv1.StatefulSet{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:        instanceGroup.NameSanitized(),
